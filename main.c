2a5d1efa31ff496575ba8617e35a4b9542fdfed6
Adam Lawrence <adam@impostertwo.com>
2024-04-24 12:56:32 -0500

    idk

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-void main() {
+int main() {
     char coolString[] = "Hi \n";
     int coolNum = 15;
     float pi = 3.14159265;
+    char coolChar = 'D';
+    double pi2 = 3.1415926535897932385;
 
 
 
     printf(coolString);
-    printf("%d",coolNum, "\n");
-    printf("%d", pi, "\n");
+    printf("%d\n",coolNum);
+    printf("%f\n", pi);
+    printf("%c\n", coolChar);
+    printf("My number is %d and my letter is %c\n", coolNum, coolChar);
+    printf("%lf\n", pi2);
+    return 0;
 }
