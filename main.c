4d8918b915b11418e03602f1716c46872d67d284
Adam Lawrence <adam@impostertwo.com>
2024-04-24 12:45:01 -0500

    idk

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+void main() {
+    char coolString[] = "Hi \n";
+    int coolNum = 15;
+    float pi = 3.14159265;
+
+
+
+    printf(coolString);
+    printf("%d",coolNum, "\n");
+    printf("%d", pi, "\n");
+}
