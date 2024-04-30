2f167ea7752b80d0d58a362fc1d2aa2eb9218eae
Adam Lawrence <adam@impostertwo.com>
2024-04-29 21:32:05 -0500

    sever

--- webserver/sever.c
+++ webserver/sever.c
@@ -5,7 +5,6 @@
 #include <unistd.h>
 #include <netinet/in.h>
 
-
 void main() {
     int s = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr = {
@@ -16,21 +15,20 @@ void main() {
 
     bind(s, &addr, sizeof(addr));
 
-	listen(s, 10);
+    listen(s, 10);
 
-	int client_fd = accept(s, 0, 0);
+    int client_fd = accept(s, 0, 0);
 
-	char buffer[256] = {0};
-	recv(client_fd, buffer, 256, 0);
+    char buffer[256] = {0};
+    recv(client_fd, buffer, 256, 0);
 
-	// GET /file.html ....
+    // GET /file.html .....
 
-	char* f = buffer + 5;
-	*strchr(f, " ") = 0;
-
-	int opened_fd = open(f, O_RDONLY);
-	sendfile(client_fd, opened_fd, 0, 256);
-	close(opened_fd);
-	close(client_fd);
-	close(s);
+    char* f = buffer + 5;
+    *strchr(f, ' ') = 0;
+    int opened_fd = open(f, O_RDONLY);
+    sendfile(client_fd, opened_fd, 0, 47);
+    close(opened_fd);
+    close(client_fd);
+    close(s);
 }
