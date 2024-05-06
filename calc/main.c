c98d30c9e2a1da00ad95b207a17b3dae39210128
Adam Lawrence <adam@impostertwo.com>
2024-05-06 13:19:45 -0500

    idk

--- calc/main.c
+++ calc/main.c
@@ -1,38 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double main(int argc, char** argv[]) {
+int main(int argc, int* argv[]) {
 
-    double result;
-    
-    printf("%s\n", argv[2]);
-    printf("%i\n", argc);
+    int result;
+    int* x = argv[0];
 
-    if () {
+    int* op = argv[1];
+    int* y = argv[2];
+
+
+    if ( !argc == 4 ) {
         printf(" Enter An Math Equation Ex: 1+4");
         return 1;
     }
 
+    
 
-    switch (argv[])
-    {
-    case 42:
-        result = argv[1] *;
-        break;
-    case 43:
-        result = x + z;
-        break;
-    case 45:
-        result = x - z;
-        break;
-    case 47:
-        result = x / z;
-        break;  
-    default:
-        break;
-        printf("Invaild Expression! \n");
-        return 1;
-    }
-    printf("%d", result);
     return 0; 
 }
+
---
+++ calc/new.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char test = 65;
+    char test2 = "A";
+
+    if ( test == test2){
+        printf("%c", test);
+    }
+}
