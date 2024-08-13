eb9f1f78c4a4847de1a110d4eb0f9b4793d8335b
Adam Lawrence <adam@impostertwo.com>
2024-08-12 21:00:12 -0500

    d

--- memory/linkedlist.c
+++ memory/linkedlist.c
@@ -1,1 +1,25 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int number;
+    struct node* next;
+} node;
+
+int main(int argc, char* argv[]){
+    node *list = NULL;
+
+    for (int i = 1; i < argc; i++){
+        int number = atoi(argv[i])
+
+        node *n = malloc(sizeof(node));
+        if(n == NULL){return 0;}
+
+        n->number =number;
+        n->next = list;
+        list = n;
+    }
+
+
+    return 0;
+}
