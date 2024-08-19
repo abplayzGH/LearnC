2a4dc7748edf5f7b76da782578d3426f05699f4c
Adam Lawrence <adam@impostertwo.com>
2024-08-19 14:19:22 -0500

    finish?

--- memory/linkedlist.c
+++ memory/linkedlist.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]){
     node *list = NULL;
 
     for (int i = 1; i < argc; i++){
-        int number = atoi(argv[i])
+        int number = atoi(argv[i]);
 
         node *n = malloc(sizeof(node));
         if(n == NULL){return 0;}
@@ -19,6 +19,14 @@ int main(int argc, char* argv[]){
         n->next = list;
         list = n;
     }
+
+
+    node *ptr = list;
+    while (ptr != NULL){
+        printf("%i\n", ptr->number);
+        ptr = ptr->next;
+    }
+    
 
 
     return 0;
